a9985e926cb77b542826313ecdd46adab953878f
RonyHirsch <rony.hirschhorn@gmail.com>
2024-03-13 21:40:55 +0200

    updated version

--- plotter.py
+++ plotter.py
@@ -111,3 +111,99 @@ def plot_raincloud(df, data_col_name, group_col_name, group_order, group_color_d
     del figure
     plt.close()
     return
+
+
+
+
+def plot_raincloud_connected(df, data_col_name, group_col_name, group_order, group_color_dict, save_path,
+                   save_name, y_title, x_title, group_name_dict=None, marker_size=50, marker_alpha=0.25,
+                   marker_spread=0.2, group_spacing=0.5, violin_width=0.35, violin_alpha=0.65,
+                   ymin=0.5, ymax=1.05, yskip=0.1):
+
+
+
+    # X axis params
+    stim_xs = {item: idx * group_spacing for idx, item in enumerate(group_order)}
+    x_list = list()
+    y_list = list()
+    left_flag = True
+    for group in group_order:
+        if group_col_name is not None:
+            df_group = df[df[group_col_name] == group]
+        else:
+            df_group = df
+        x_loc = stim_xs[group]
+        y_vals = df_group[data_col_name].tolist()
+        # plot violin
+        violin = plt.violinplot(y_vals, positions=[x_loc], widths=violin_width, showmeans=True, showextrema=False, showmedians=False)
+
+
+        if left_flag:
+            # make it a half-violin plot (only to the LEFT of center)
+            for b in violin['bodies']:
+                # get the center
+                m = np.mean(b.get_paths()[0].vertices[:, 0])
+                # modify the paths to not go further right than the center
+                b.get_paths()[0].vertices[:, 0] = np.clip(b.get_paths()[0].vertices[:, 0], -np.inf, m)
+                b.set_color(group_color_dict[group])
+                b.set_alpha(violin_alpha)
+                b.set_edgecolor(group_color_dict[group])
+        else:
+            # make it a half-violin plot (only to the RIGHT of center)
+            for b in violin['bodies']:
+                # get the center
+                m = np.mean(b.get_paths()[0].vertices[:, 0])
+                # modify the paths to not go further left than the center
+                b.get_paths()[0].vertices[:, 0] = np.clip(b.get_paths()[0].vertices[:, 0], m, np.inf)
+                b.set_color(group_color_dict[group])
+                b.set_alpha(violin_alpha)
+                b.set_edgecolor(group_color_dict[group])
+
+        # change the color of the mean lines (showmeans=True)
+        violin['cmeans'].set_color("black")
+        violin['cmeans'].set_linewidth(4)
+
+        # control the length
+        m = np.mean(violin['cmeans'].get_paths()[0].vertices[:, 0])
+        violin['cmeans'].get_paths()[0].vertices[:, 0] = np.clip(violin['cmeans'].get_paths()[0].vertices[:, 0], -np.inf, m)
+
+        # then scatter
+        if left_flag:
+            scat_x = (np.ones(len(y_vals)) * (x_loc + marker_spread/3.5)) + (np.random.rand(len(y_vals)) * marker_spread)
+        else:
+            scat_x = (np.ones(len(y_vals)) * (x_loc - marker_spread / 3.5)) - (np.random.rand(len(y_vals)) * marker_spread)
+        plt.scatter(x=scat_x, y=y_vals, marker="o", color=group_color_dict[group], s=marker_size, alpha=marker_alpha, edgecolor=group_color_dict[group])
+
+        # because we want to add subject lines:
+        x_list.append(scat_x)
+        y_list.append(y_vals)
+
+        # reset flag:
+        left_flag = False
+
+    # Now subject lines
+    for sub in range(len(x_list[0])):
+        m_list = [x_list[l][sub] for l in range(len(x_list))]
+        n_list = [y_list[l][sub] for l in range(len(x_list))]
+        plt.plot(m_list, n_list, color="lightgray", linewidth=0.75, alpha=1)
+
+    # cosmetics
+    if group_name_dict:
+        plt.xticks(ticks=[(idx * group_spacing) for idx, item in enumerate(group_order)],
+                   labels=[group_name_dict[item] for item in group_order], fontsize=TICK_SIZE-2)
+    else:
+        plt.xticks(ticks=[(idx * group_spacing) for idx, item in enumerate(group_order)],
+                   labels=[item for item in group_order], fontsize=TICK_SIZE-2)
+    plt.yticks([y for y in np.arange(ymin, ymax, yskip)], fontsize=TICK_SIZE-2)
+    plt.title("")
+    plt.ylabel(y_title, fontsize=AXIS_SIZE-2, labelpad=LABEL_PAD)
+    plt.xlabel(x_title, fontsize=AXIS_SIZE-2, labelpad=LABEL_PAD+2)
+
+
+    # save plot
+    figure = plt.gcf()  # get current figure
+    figure.set_size_inches(15, 12)
+    plt.savefig(os.path.join(save_path, f"{save_name}.svg"), format="svg", dpi=1000, bbox_inches='tight', pad_inches=0.01)
+    del figure
+    plt.close()
+    return
--- revision_analyses.py
+++ revision_analyses.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import functools as ft
 import process_raw_data
 
 """
@@ -10,28 +10,6 @@ Manage extra processing experimental data as part of the revision process of the
 """
 
 __author__ = "Rony Hirschhorn"
-
-
-
-"""
-
-FRAME RATE ANALYSIS
-
-"""
-def count_non_empty(row):
-    return sum(row[col] == row[col] for col in process_raw_data.WORDS)
-
-
-def frame_rate_analysis_prep(path_to_experiment_df, save_path):
-    data = pd.read_csv(path_to_experiment_df)
-    data['num_of_words'] = data.apply(lambda row: count_non_empty(row), axis=1)
-    data.to_csv(os.path.join(save_path, "experiment_df_words_corrected_count.csv"), index=False)
-
-    # stats
-    data['participant'] = pd.to_numeric(data['participant'])
-    stats = data.groupby('participant').mean(numeric_only=True)
-    stats.describe().to_csv(os.path.join(save_path, "experiment_df_words_corrected_stats.csv"))
-    return
 
 
 
@@ -65,10 +43,210 @@ def confidence_IA_corr(path_to_iaconf_df, save_path, save_name,title):
     return
 
 
+"""
+Existence analyses
+"""
+
+
+def ia_in_image(word, ia_word_list):
+    if word != word:  # word is empty - no word was actually provided
+        return 0
+    if word in ia_word_list:  # if a word is rare, it doesn't have an IA score
+        return 1
+    return 0
+
+
+def count_ia(row, non_rare_df):
+    curr_image = row['stim_name'].split('/')[-1].split('.')[0]
+    non_rare_df_image = non_rare_df[non_rare_df['image'] == curr_image]
+    ia_word_list = non_rare_df_image['word'].tolist()
+    have_ia = sum(ia_in_image(row[col], ia_word_list) for col in process_raw_data.WORDS)
+    return have_ia
+
+
+def not_exists_in_image(word, curr_image, exist_df, rare_word_list):
+    if word != word:  # the word is nan (i.e., not provided a word to begin with)
+        return 0
+    if word in rare_word_list:  # the word is rare, has no IA score, therefore not tagged as existing or non-existing
+        return 0
+    relevant_df = exist_df[exist_df['image'] == curr_image]
+    relevant_entry = relevant_df[relevant_df['word'] == word]  # this should never be empty at this stage
+    if relevant_entry.empty:  # some other error, shouldn't happen
+        return 0
+    exist = relevant_entry['existInImage'].item()
+    if exist != exist:
+        return 0
+    """
+    else, the word either got 1 (exists in image) or 0 (non). As we want to sum the NON existing, we need to return 
+    '1' if the word DOESN'T exist
+    """
+    return int(exist) ^ 1
+
+
+def count_nonexist(row, exist_df, rare_df):
+    curr_image = row['stim_name'].split('/')[-1].split('.')[0]
+    rare_df_image = rare_df[rare_df['image'] == curr_image]
+    rare_word_list = rare_df_image['word'].tolist()
+    existing = sum(not_exists_in_image(row[col], curr_image, exist_df, rare_word_list) for col in process_raw_data.WORDS)
+    return existing
+
+
+def nonexistent_posthoc_analysis(version, exist_file, lemmatized_file, rare_file, save_path):
+    # A dataframe where every word with a word-IA in the image is marked as existing or not
+    exist_df = pd.read_csv(exist_file)
+    exist_df_relevant = exist_df[exist_df['version'] == version]
+
+    # A dataframe where every word provided to a given image is marked as rare (=doesn't have a word IA) or not (has word IA)
+    rare_df = pd.read_csv(rare_file)
+    rare_df['image'] = rare_df['image'].apply(lambda x: x.split('/')[-1].split('.')[0])
+    # we are only interested in this df to check rare words, therefore we'll only leave those which are tagged as such
+    rare_df_onlynorare = rare_df[rare_df['word is rare'] == 0]  # these words have IA scores
+    rare_df_onlyrare = rare_df[rare_df['word is rare'] == 1]  # these words do not have IA scores
+
+    # A dataframe where we have all words provided by each participant to each image
+    lemmatized_df = pd.read_csv(lemmatized_file)
+    lemmatized_df = lemmatized_df.loc[:, ~lemmatized_df.columns.str.contains('^Unnamed')]  # delete Unnamed columns
+    lemmatized_df['num_of_IA'] = lemmatized_df.apply(lambda row: count_ia(row, rare_df_onlynorare), axis=1)
+    lemmatized_df['num_of_nonexist'] = lemmatized_df.apply(lambda row: count_nonexist(row, exist_df_relevant, rare_df_onlyrare), axis=1)
+    lemmatized_df['nonexist_ratio'] = lemmatized_df['num_of_nonexist'] / lemmatized_df['num_of_IA']
+    lemmatized_df.to_csv(os.path.join(save_path, "experiment_df_words_lemmatized_nonExistRatio.csv"), index=False)
+    return
+
+
+
+
+"""
+Reviewer 1: words with IA but w/o any connection to the image
+"""
+COL_EXIST = "existInImage"
+COL_NOEXIST_GISTONLY = "nonexist: right gist non-existent item"
+COL_NOEXIST_CONFUSED = "nonexist:  confusion"
+COL_NOEXIST_UNRELATED = "nonexist: no connection at all"
+COL_NOEXIST_CONCEPTUAL = "conceptual word"
+
+EXP_VERSION = "version"
+IMAGE_NAME_COL = "image"
+
+
+def existence_analysis(exist_file, save_path):
+    # A dataframe where every word with a word-IA in the image is marked as existing or not
+    exist_df = pd.read_csv(exist_file)
+
+    # how many words with IA are there in for each image (in each version)
+    ia_counts = exist_df.groupby([EXP_VERSION, IMAGE_NAME_COL]).count()["word"].reset_index(drop=False)
+    ia_counts.rename(columns={"word": "words with IA"}, inplace=True)
+
+    # how many words with IA are there in each existence type (1's in one of the crucial columns
+    """
+    "count" counts both "1"s and "0"s (and ignores NAs). 
+    We want to sum ONLY the "1"s, those that were tagged as such (so we use "sum")
+    """
+    # exist
+    exist_counts = exist_df.groupby([EXP_VERSION, IMAGE_NAME_COL]).sum()[COL_EXIST].reset_index(drop=False)
+    # right gist, but not in image
+    gist_counts = exist_df.groupby([EXP_VERSION, IMAGE_NAME_COL]).sum()[COL_NOEXIST_GISTONLY].reset_index(drop=False)
+    # wrong gist (confusion)
+    confusion_counts = exist_df.groupby([EXP_VERSION, IMAGE_NAME_COL]).sum()[COL_NOEXIST_CONFUSED].reset_index(drop=False)
+    # completely unrelated
+    unrelated_counts = exist_df.groupby([EXP_VERSION, IMAGE_NAME_COL]).sum()[COL_NOEXIST_UNRELATED].reset_index(drop=False)
+    # conceptual
+    conceptual_counts = exist_df.groupby([EXP_VERSION, IMAGE_NAME_COL]).sum()[COL_NOEXIST_CONCEPTUAL].reset_index(drop=False)
+
+    # merge them
+    df_list = [ia_counts, exist_counts, gist_counts, confusion_counts, unrelated_counts, conceptual_counts]
+    word_stats_df = ft.reduce(lambda left, right: pd.merge(left, right, on=[EXP_VERSION, IMAGE_NAME_COL]), df_list)
+    # convert floats to ints
+    cols = [COL_EXIST, COL_NOEXIST_GISTONLY, COL_NOEXIST_CONFUSED, COL_NOEXIST_UNRELATED, COL_NOEXIST_CONCEPTUAL]
+    word_stats_df[cols] = word_stats_df[cols].astype(int)
+
+    """
+    sanity check: the 'zeros' column should be all zeros as both 'sum' and 'ia' columns should contain all the IA words
+    of this image (no more no less). ---> "df" should be empty
+    """
+    word_stats_df['sum'] = word_stats_df[cols].sum(axis=1)
+    word_stats_df['zeros'] = word_stats_df['sum'] - word_stats_df['words with IA']
+    df = word_stats_df[word_stats_df['zeros'] != 0]
+
+    # after we are confident that we have all words tagged (and no double tagging), we can calculate %s
+    word_stats_df.to_csv(os.path.join(save_path, "word_counts_types_raw.csv"), index=False)
+    # save converted
+    word_stats_df[cols] = word_stats_df[cols].div(word_stats_df['words with IA'], axis=0) * 100
+    word_stats_df.to_csv(os.path.join(save_path, "word_counts_pctgs_types.csv"), index=False)
+
+    # now do stats for %s
+    word_stats_df_noimage = word_stats_df.drop(columns=[IMAGE_NAME_COL], inplace=False)
+    mean_df = word_stats_df_noimage.groupby([EXP_VERSION]).mean()
+    mean_df.to_csv(os.path.join(save_path, "word_counts_pctgs_mean.csv"), index=True)
+    std_df = word_stats_df_noimage.groupby([EXP_VERSION]).std()
+    std_df.to_csv(os.path.join(save_path, "word_counts_pctgs_sd.csv"), index=True)
+
+    """
+    Now, break it down: for each type, what's the average word IA score. As our results were about word IAs, 
+    no need to collapse per image like we did above to get these stats. 
+    """
+    results = []
+    raw_dfs = []
+    for version in exist_df.version.unique().tolist():
+        version_df = exist_df[exist_df[EXP_VERSION] == version]
+        result = {}
+        for col in cols:
+            col_IA = version_df[version_df[col] == 1]  # only for this existence category, all words
+            col_IA_mean = col_IA['word IA'].mean()
+            col_IA_sd = col_IA['word IA'].std()
+            result[col] = {f'Mean IA {version}': col_IA_mean, f'Std IA {version}': col_IA_sd}
+            # for aggregating later
+            col_IA.reset_index(inplace=True, drop=True)
+            col_IA['status'] = col
+            relevant_cols = ['word', 'word IA', 'version', 'image', 'status']
+            col_IA = col_IA.loc[:, relevant_cols]
+            raw_dfs.append(col_IA)
+        result_df = pd.DataFrame(result)
+        results.append(result_df)
+    # actual data
+    raw_df = pd.concat(raw_dfs)
+    raw_df.to_csv(os.path.join(save_path, f"word_IA_per_existence_data.csv"), index=False)
+    # stats
+    results_df = pd.concat(results)
+    results_df.to_csv(os.path.join(save_path, f"word_IA_per_existence_stats.csv"), index=True)
+    return
+
+
+
+
+
+"""
+Slider (control exp. B)
+"""
+
+PATH_INTACT = "resources/stimuli/exp/orig/"
+PATH_BLURRED = "resources/stimuli/exp/blur10/"
+COL_STIM = "exp_stim"
+
+def get_image(row):
+    image = row[COL_STIM].replace(PATH_INTACT, "").replace(PATH_BLURRED, "").replace(".jpg", "")
+    return image
+
+def get_version(row):
+    if PATH_INTACT in row[COL_STIM]:
+        return "intact"
+    return "blurred"
+
+def slider_analysis(sub_df_file, save_path):
+    data = pd.read_csv(sub_df_file)
+    data['image'] = data.apply(lambda row: get_image(row), axis=1)
+    data['version'] = data.apply(lambda row: get_version(row), axis=1)
+    relevant_data = data[['participant', 'image', 'version', 'trial_slider.response', 'trial_slider.rt']]
+    relevant_data.to_csv(os.path.join(save_path, "subject_df_perImage.csv"), index=False)
+    relevant_data.drop(columns=['participant'], inplace=True)
+    relevant_data[['trial_slider.response', 'trial_slider.rt']] = relevant_data[['trial_slider.response', 'trial_slider.rt']].astype(float)
+    grouped = relevant_data.groupby(['image', 'version']).mean().reset_index(drop=False)
+    grouped.to_csv(os.path.join(save_path, "subject_df_perImage_grouped.csv"), index=False)
+    pivot = grouped.pivot(index='image', columns='version', values='trial_slider.response').reset_index(drop=False)
+    pivot.to_csv(os.path.join(save_path, "slider_per_image_version.csv"), index=False)
+    return
+
+
 if __name__ == "__main__":
-    #frame_rate_analysis_prep(path_to_experiment_df=r"..\experiment_df_words_corrected.csv",
-    #                    save_path=r"..")
-    confidence_IA_corr(path_to_iaconf_df=r"..word_IA_confidence.csv",
-                       save_path=r"..",
-                       save_name="orig_67",
-                       title="Original Study 67ms")
+    slider_analysis(
+        sub_df_file=r"..\subject_df.csv",
+        save_path=r"..\processed")
